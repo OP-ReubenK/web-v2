5c422f997d5a9e43edb18dad644f299372dc4c06
Sasnz <r.koks@hotmail.co.nz>
2021-11-02 12:51:38 +1300

    Multiple css changes

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+document.querySelector(".cart").addEventListener("click", (event) => {
+    document.querySelector(".cartPopup").classList.toggle("open");
+})
+
+document.querySelector(".menu").addEventListener("click", (event) => {
+    document.querySelector(".menuPopup").classList.toggle("open");
+})
