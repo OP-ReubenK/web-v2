c26b7f3f7dbea329f2c43fe08b0d31188c443ffd
Sasnz <r.koks@hotmail.co.nz>
2021-11-02 14:33:22 +1300

    many css updates. some blocking style removed

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,29 @@ document.querySelector(".menu").addEventListener("click", (event) => {
 document.querySelector(".menuClose").addEventListener("click", (event) => {
     document.querySelector(".menuPopup").classList.toggle("open");
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    let mousePosX = 0,
+        mousePosY = 0,
+        mouseCircle = document.querySelector('#mouse-circle');
+
+    document.onmousemove = (e) => {
+        mousePosX = e.pageX;
+        mousePosY = e.pageY;
+    }
+
+    let delay = 6,
+        revisedMousePosX = 0,
+        revisedMousePosY = 0;
+
+    function delayMouseFollow() {
+        requestAnimationFrame(delayMouseFollow);
+
+        revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
+        revisedMousePosY += (mousePosY - revisedMousePosY) / delay; 
+
+        mouseCircle.style.top = revisedMousePosY + 'px';
+        mouseCircle.style.left = revisedMousePosX + 'px';
+    }
+    delayMouseFollow();
+});
