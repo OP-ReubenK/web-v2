34e8cf0b4b301d8d27e4620ad8daf2a35088b71f
Sasnz <r.koks@hotmail.co.nz>
2021-11-02 13:16:53 +1300

    Close buttons on menu drops working

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,14 @@ document.querySelector(".cart").addEventListener("click", (event) => {
     document.querySelector(".cartPopup").classList.toggle("open");
 })
 
+document.querySelector(".cartClose").addEventListener("click", (event) => {
+    document.querySelector(".cartPopup").classList.toggle("open");
+})
+
 document.querySelector(".menu").addEventListener("click", (event) => {
+    document.querySelector(".menuPopup").classList.toggle("open");
+})
+
+document.querySelector(".menuClose").addEventListener("click", (event) => {
     document.querySelector(".menuPopup").classList.toggle("open");
 })
